8c454db53766bb842b290e22aca5ea3b029167a6
Mads Bondo Dydensborg <mads@dydensborg.dk>
2010-11-01 20:32:21 +0000

    Encapsulated, added toRentalHtml method

--- contractitem.cpp
+++ contractitem.cpp
@@ -9,21 +9,28 @@ ContractItem::ContractItem() : m_id( -1 ) {
     Logger log("ContractItem::ContractItem() : m_id( -1 )");
 }
 
-ContractItem::ContractItem(qlonglong contract_id, const QString &item_id,
+ContractItem::ContractItem(qlonglong contract_id, const Item &item,
                            const QString &rentalgroup, qlonglong price,
                            const QString &note )
                                : m_id( -1 ), m_contract_id( contract_id ),
-                               m_item_id( item_id ),
+                               m_item( item ),
                                m_state( DB::ContractItem::State::booked ),
                                m_rentalgroup( rentalgroup ),
                                m_price( price ), m_note( note ) {
-    Logger log("ContractItem::ContractItem(qlonglong contract_id, const QString &item_id, const QString &rentalgroup, qlonglong price, const QString &note)");
+    Logger log("ContractItem::ContractItem(qlonglong contract_id, const Item &item, const QString &rentalgroup, qlonglong price, const QString &note)");
     log.stream()
             << "m_id: '" << m_id
             << "', m_contract_id: '" << m_contract_id
-            << "', m_item_id: '" << m_item_id
+            << "', m_item_id: '" << m_item.getId()
             << "', m_state: '" << m_state
             << "', m_rentalgroup '" << m_rentalgroup
             << "', m_price '" << m_price
             << "', m_note '" << m_note << "'";
+}
+
+QString ContractItem::toRentalHtml() const {
+    Logger log("QString ContractItem::toRentalHtml() const");
+    return QString( "<tr><td>%0</td><td align=\"right\">%1</td></tr>\n" )
+            .arg( m_item.toRentalHtml() )
+            .arg( m_price );
 }
--- contractitem.h
+++ contractitem.h
@@ -1,18 +1,15 @@
 #ifndef CONTRACTITEMS_H
 #define CONTRACTITEMS_H
 
+// Qt
 #include <QString>
+
+// App
+#include "item.h"
 
 class ContractItem
 {
 public:
-    qlonglong m_id;
-    qlonglong m_contract_id;
-    QString m_item_id;
-    QString m_state;
-    QString m_rentalgroup;
-    qlonglong m_price;
-    QString m_note;
 
     /** \brief Default constructor.
       *
@@ -21,16 +18,42 @@ public:
 
     /** \brief Create a ContractItem from certain set of values
       * \param contract_id The id of the contract
-      * \param item_id The The id of the item
+      * \param item The item for this contract
       * \param rentalgroup The rental group of the item on this contract
       * \param price The price of the item on this contract
       * \param note A note for the item
       *
       * This constructor construct with the given values, and state = 'booked'.
       * It is *not* written to the database. Use insert for that */
-    ContractItem( qlonglong contract_id, const QString & item_id,
+    ContractItem( qlonglong contract_id, const Item & item,
                   const QString & rentalgroup, qlonglong price,
                   const QString & note );
+
+    /** \brief Return some HTML suitable to show in the rental dialog
+      * \returns HTML suitable for show in the rental dialog */
+    QString toRentalHtml() const;
+
+private:
+    /** \brief Database field: Id of the contract item in the database */
+    qlonglong m_id;
+    /** \brief Database field: Id of the contract in the database */
+    qlonglong m_contract_id;
+    /** \brief Copy of item assigned to this contractitem.
+      * Only item id is stored in database */
+    Item m_item;
+    /** \brief State of contractitem
+      *
+      * booked: Still booking/reserved for this contract
+      * out: Out - that is, in the care of the hirer
+      * returned: Returned. */
+    QString m_state;
+    /** \brief Database field: The rental group registered for this item on this contract */
+    QString m_rentalgroup;
+    /** \brief Database field: The price registered for this item on this contract */
+    qlonglong m_price;
+    /** \brief Database field: A note */
+    QString m_note;
+
 
 };
 
