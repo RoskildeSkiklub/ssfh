e694de94950f0648ad0c77c444d70ec96f1d5e64
Mads Bondo Dydensborg <mads@dydensborg.dk>
2011-08-13 20:49:37 +0000

    Needs log initialized

--- unittest/main.cpp
+++ unittest/main.cpp
@@ -1,13 +1,21 @@
 #include "AutoTest.h"
 #include <QDebug>
 
-#if 1
+// Lots of the classes require the log module to be initialized
+#include "log.h"
+
+#if 0
+
 // This is all you need to run all the tests
 TEST_MAIN
 #else
 // Or supply your own main function
 int main(int argc, char *argv[])
 {
+    // Need to set up logging...
+    Log::Log::init( "unittest.log" );
+
+    // Actually run the tests
     int failures = AutoTest::run(argc, argv);
     if (failures == 0)
     {
