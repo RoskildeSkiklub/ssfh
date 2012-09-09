a145443f89297b98e39a117fd9246127d4bd2863
Mads Bondo Dydensborg <mads@dydensborg.dk>
2012-09-09 16:35:49 +0000

    Added some docs for the perform dialog class

--- src/performstatusdialog.h
+++ src/performstatusdialog.h
@@ -7,6 +7,14 @@ namespace Ui {
 class PerformStatusDialog;
 }
 
+/** \brief A dialog window to help in performing status
+  *
+  * This dialog helps with performing status, that is, scanning all items
+  * to figure out which items are present, and eventually, which are missing.
+  * (A report is needed to figure out which are missing).
+  *
+  * The dialog also tracks if some items are scanned, which are not marked as in.
+  * These can be printed, in order to allow returning them at a later point. */
 class PerformStatusDialog : public QDialog
 {
     Q_OBJECT
@@ -16,17 +24,22 @@ public:
     ~PerformStatusDialog();
     
 public slots:
+    /** \brief signalled, whenever an item is scanned */
     void scan_item( const QString & item_id );
 
 signals:
+    /** \brief Emitted when an item was succesfully scanned */
     void item_scanned();
 
 private slots:
 
+    /** \brief Closes the dialog */
     void on_input_close_pushButton_clicked();
 
+    /** \brief Allows to save item_ids for items that are present but not marked as in */
     void on_input_save_pushButton_clicked();
 
+    /** \brief Allows to print item_ids for items that are present but not marked as in */
     void on_input_print_pushButton_clicked();
 
 private:
